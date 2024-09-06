4cdea09ccea38af4e32a7cdfd190a045b6d36f4f
Myseie <123580372+Myseie@users.noreply.github.com>
2024-09-06 10:22:03 +0200

    Add files via upload

---
+++ Palindrome/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Skriv ett ord här: ");
+string input = Console.ReadLine();
+
+char[] charArray = input.ToCharArray();
+
+Array.Reverse(charArray);
+
+string reversed = new string(charArray);
+
+if (input == reversed)
+{
+    Console.WriteLine("Palindrom");
+}
+else
+{
+    Console.WriteLine("Inte en palindrom");
+}
+
